567664eabc2a59dda336f7726df6f850ad100a2a
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-25 18:31:07 +0530

    Initial commit

---
+++ src/test/java/com/aryak/kerberos/SpringKerberosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryak.kerberos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKerberosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
